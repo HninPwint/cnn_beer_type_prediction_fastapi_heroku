43384f5ed1eadff35d8520b9647bb2c5aa14f566
HninPwint <hninptgmail.com>
2021-03-03 11:27:25 +1100

    Git Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a ML model to predict the type of beer and to deploy to Production',
+    author='Hnin',
+    license='MIT',
+)
